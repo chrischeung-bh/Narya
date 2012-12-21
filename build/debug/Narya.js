85e4f5e0dad67e2f15ef235f7c602eaa1d5e1549
Zeke Zhao | 赵望野 <zhaowyivan@vip.sina.com>
2012-12-21 19:28:11 +0800

    Add copyright

--- build/debug/Narya.js
+++ build/debug/Narya.js
@@ -3147,6 +3147,17 @@
     });
 }(this));
 
+/*!
+ * Narya.js Wandoujia 2.0 Doraemon JavaScript SDK v0.5
+ * https://github.com/wandoulabs/Narya
+ *
+ * Copyright 2010 - 2012 WandouLabs
+ * Release under GPL License.
+ * 
+ * Read sourece code? Join us! We're hiring WEB front-end engineer
+ * with curiosity like U!
+ * http://www.wandoujia.com/join
+ */
 (function (window, undefined) {
     require([
         'Doraemon/Doraemon',
